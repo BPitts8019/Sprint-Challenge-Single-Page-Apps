93978f2e3ba02fe99723040938bc07f61ba2369a
BPitts8019 <bradleyj.pitts@gmail.com>
2019-09-13 17:38:40 -0700

    Still not working, but moving on

--- src/components/TabNav.js
+++ src/components/TabNav.js
@@ -1,5 +1,4 @@
-import React from "react";
-// import { Tab, Menu, Icon } from "semantic-ui-react";
+import React, {useState, useEffect} from "react";
 import {Nav, NavItem, NavLink as RSNavLink, TabContent, TabPane} from "reactstrap";
 import { NavLink } from "react-router-dom";
 
@@ -12,28 +11,48 @@ import { NavLink } from "react-router-dom";
 // https://react.semantic-ui.com/collections/breadcrumb/
 
 export default function TabNav() {
+   const [activeTab, setActiveTab] = useState(1);
+   const clickHandler = event => {
+      event.preventDefault();
+      setActiveTab(event.target.dataset.tabId);
+   }
+
    return (
       <>
-         <Nav tabs>
-            <NavItem><RSNavLink href="/blah">Home Page</RSNavLink></NavItem>
-            <NavItem><RSNavLink href="/characters">Characters</RSNavLink></NavItem>
-            <NavItem><RSNavLink href="/locations">Locations</RSNavLink></NavItem>
-            <NavItem><RSNavLink href="/episodes">Episodes</RSNavLink></NavItem>
-         </Nav>
-         <TabContent>
-            <TabPane tabId="1">
-               <h4>Home tab</h4>
-            </TabPane>
-            <TabPane tabId="1">
-               <h4>Characters tab</h4>
-            </TabPane>
-            <TabPane tabId="1">
-               <h4>Locations tab</h4>
-            </TabPane>
-            <TabPane tabId="1">
-               <h4>Episodes tab</h4>
-            </TabPane>
-         </TabContent>
+         <ul>
+            <li><a href="/" data-tabId="1" onClick={clickHandler}>Home Page</a></li>
+            <li><a href="/characters" data-tabId="2" onClick={clickHandler}>Characters</a></li>
+            <li><a href="/locations" data-tabId="3" onClick={clickHandler}>Locations</a></li>
+            <li><a href="/episodes" data-tabId="4" onClick={clickHandler}>Episodes</a></li>
+         </ul>
+         <div className="tabStuff">
+            {(activeTab === 1)? <h4>Home Page</h4> : null}
+            {(activeTab === 2)? <h4>Characters Page</h4> : null}
+            {(activeTab === 3)? <h4>Locations Page</h4> : null}
+            {(activeTab === 4)? <h4>Episodes Page</h4> : null}
+         </div>
       </>
+      // <>
+      //    <Nav tabs>
+      //       <NavItem><RSNavLink href="/blah">Home Page</RSNavLink></NavItem>
+      //       <NavItem><RSNavLink href="/characters">Characters</RSNavLink></NavItem>
+      //       <NavItem><RSNavLink href="/locations">Locations</RSNavLink></NavItem>
+      //       <NavItem><RSNavLink href="/episodes">Episodes</RSNavLink></NavItem>
+      //    </Nav>
+      //    <TabContent>
+      //       <TabPane tabId="1">
+      //          <h4>Home tab</h4>
+      //       </TabPane>
+      //       <TabPane tabId="1">
+      //          <h4>Characters tab</h4>
+      //       </TabPane>
+      //       <TabPane tabId="1">
+      //          <h4>Locations tab</h4>
+      //       </TabPane>
+      //       <TabPane tabId="1">
+      //          <h4>Episodes tab</h4>
+      //       </TabPane>
+      //    </TabContent>
+      // </>
    );
 };

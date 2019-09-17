442d6ecadcc9db5b307d24840024fe5a1a1ec9ed
BPitts8019 <bradleyj.pitts@gmail.com>
2019-09-16 17:18:02 -0700

    Updated Location Cards using Semantic UI

--- src/components/LocationCard.js
+++ src/components/LocationCard.js
@@ -1,14 +1,25 @@
 import React from "react";
+import {Card, Icon, Image, Label} from "semantic-ui-react";
 
 export default function LocationCard({ name, type, dimension, residents }) {
    return (
-      <div className="card ui">
-         <h2>{name}</h2>
-         <ul>
-            <li>Type: {type}</li>
-            <li>Dimension: {dimension}</li>
-            <li>Residents: {residents.length}</li>
-         </ul>
-      </div>
+      <Card>
+         <Card.Content>
+            <Card.Header>{name}</Card.Header>
+            <Card.Description>{type} - {dimension}</Card.Description>
+         </Card.Content>
+         <Card.Content className="extra right aligned">
+            <Label>
+               <Icon className="user" />
+               {residents.length}
+            </Label>
+         </Card.Content>
+      </Card>
+
+
+      // <div className="card ui">
+      //    <h3 className="ui">{name}</h3>
+      //    <h5></h5>
+      // </div>
    );
 }
